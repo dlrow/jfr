29d9fca9cb83baa379aabee4a84f755200b0d7d8
ababraj <ababraj@192.168.56.1>
2019-12-25 14:47:44 +0530

    jfr

---
+++ src/main/java/GenerateReports.java
@@ -0,0 +1,96 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class GenerateReports {
+
+	public static void main(String[] args) {
+		String excelPath = "test.xlsx";
+		generateReport(excelPath);
+	}
+
+	private static void generateReport(String excelPath) {
+		Map<String, List<String>> ecids = getEcids(excelPath);
+		List<String> commandsToExecute = createCommands(ecids);
+		executeCommands(commandsToExecute);
+		System.out.println("report generated");
+	}
+
+	private static void executeCommands(List<String> commandsToExecute) {
+		Runtime rt = Runtime.getRuntime();
+		for (String command : commandsToExecute) {
+			try {
+				rt.exec(new String[] { "cmd.exe", "/c", command });
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static List<String> createCommands(Map<String, List<String>> ecids) {
+		List<String> commands = new ArrayList<>();
+		for (Entry<String, List<String>> entry : ecids.entrySet()) {
+
+			for (String ecid : entry.getValue()) {
+				String dir = "Reports\\" + entry.getKey() + "\\" + ecid;
+				new File(dir).mkdirs();
+				String cmd = "java -jar " + "JFRParser.jar" + " /jfr " + entry.getKey() + " /o " + dir + "\\ /ecid \""
+						+ ecid + "\"";
+				commands.add(cmd);
+			}
+		}
+		return commands;
+	}
+
+	protected static Map<String, List<String>> getEcids(String excelPath) {
+		Map<String, List<String>> ecids = new HashMap<>();
+		Map<Integer, String> latestJfrFoundForColumn = new HashMap<>();
+		try {
+			InputStream excelFile = new FileInputStream(new File(excelPath));
+			Workbook workbook = new XSSFWorkbook(excelFile);
+			Sheet datatypeSheet = workbook.getSheetAt(0);
+			Iterator<Row> iterator = datatypeSheet.iterator();
+
+			while (iterator.hasNext()) {
+				Row currentRow = iterator.next();
+				Iterator<Cell> cellIterator = currentRow.iterator();
+				while (cellIterator.hasNext()) {
+					Cell currentCell = cellIterator.next();
+					if (currentCell.getCellTypeEnum() == CellType.STRING) {
+						String curr = currentCell.getStringCellValue();
+						if (curr.indexOf("jvm_flightRecording") > -1 && curr.indexOf(".jfr") > -1) {
+							latestJfrFoundForColumn.put(currentCell.getColumnIndex(), curr);
+						} else if (curr.length() == "005aXu_I7HK3z035Rnh8id0003kz0001jf".length()) {
+							String jfrName = latestJfrFoundForColumn.get(currentCell.getColumnIndex());
+							if (!ecids.containsKey(jfrName))
+								ecids.put(jfrName, new ArrayList<>());
+							ecids.get(jfrName).add(curr);
+							ReadReportAndWriteToExcel.ecidPos.put(curr,
+									new ecidCell(currentCell.getRowIndex(), currentCell.getColumnIndex()));
+						}
+					}
+
+				}
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ecids;
+	}
+
+}
---
+++ src/main/java/ReadReportAndWriteToExcel.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+class ecidCell {
+	int x;
+	int y;
+
+	public ecidCell(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+}
+
+public class ReadReportAndWriteToExcel {
+	static Map<String, ecidCell> ecidPos = new HashMap<>();
+
+	public static void main(String[] args) {
+		String excelPath = "test.xlsx";
+		populateTime(excelPath);
+	}
+
+	private static void populateTime(String excelPath) {
+		Map<String, List<String>> ecids = GenerateReports.getEcids(excelPath);
+		Map<String, String> ecidTime = getTimeForEachEcids(ecids);
+		writeToExcel(ecidTime, excelPath);
+	}
+
+	private static Map<String, String> getTimeForEachEcids(Map<String, List<String>> ecids) {
+		Map<String, String> ecidTime = new HashMap<String, String>();
+		for (Entry<String, List<String>> entry : ecids.entrySet()) {
+
+			for (String ecid : entry.getValue()) {
+
+				try {
+					BufferedReader in = new BufferedReader(new FileReader("Reports\\"+entry.getKey() + "\\" + ecid + "\\.html"));
+					String str;
+					while ((str = in.readLine()) != null) {
+						if (str.contains("Duration")) {
+							int indexOfDuration = str.indexOf("Duration");
+							int begin = str.indexOf("text", indexOfDuration);
+							int end = str.indexOf("td", begin);
+							String time = str.substring(begin + 6, end - 2);
+							ecidTime.put(ecid, time);
+						}
+					}
+					in.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		return ecidTime;
+	}
+
+	private static void writeToExcel(Map<String, String> ecidTime, String excelPath) {
+		try {
+
+			FileInputStream excelFile = new FileInputStream(new File(excelPath));
+			Workbook workbook = new XSSFWorkbook(excelFile);
+			Sheet datatypeSheet = workbook.getSheetAt(0);
+			for (Entry<String, String> entry : ecidTime.entrySet()) {
+				ecidCell currEcidCell = ecidPos.get(entry.getKey());
+				Cell cell = datatypeSheet.getRow(currEcidCell.x).createCell(currEcidCell.y + 1);
+				cell.setCellValue(entry.getValue());
+			}
+
+			FileOutputStream outFile = new FileOutputStream(new File(excelPath));
+			workbook.write(outFile);
+			outFile.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
